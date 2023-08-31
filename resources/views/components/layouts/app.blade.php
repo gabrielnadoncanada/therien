56bec61f0bc656d36061ac91e3127dc8a65143ec
gabrielnadoncanada <gabrielnadoncanada@gmail.com>
2023-08-30 22:56:41 -0400

    json >> text

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -11,7 +11,7 @@
 <body class="bg-foreground ">
 <x-sections.site.header class="absolute z-10 w-full"></x-sections.site.header>
 <main id="main" class="overflow-hidden">{{ $slot }}</main>
-<x-sections.site.contact class="mt-[229px] max-w-[1280px] bg-primary py-[96px] px-[122px] rounded-[500px] mx-auto  flex justify-between gap-x-[140px]"></x-sections.site.contact>
+<x-sections.site.contact class="mt-[229px] max-w-[1280px] flex-wrap bg-primary py-[96px] px-[122px] rounded-[500px] mx-auto  flex justify-between gap-x-[140px]"></x-sections.site.contact>
 <x-sections.site.footer class="mt-[286px] border-t border-primary"></x-sections.site.footer>
 @vite('resources/js/app.js')
 </body>
