0ae6ac79630dd92325a7c955e2d0212e9a864228
gabrielnadoncanada <gabrielnadoncanada@gmail.com>
2023-08-30 22:50:25 -0400

    json >> text

--- resources/views/components/sections/site/hero.blade.php
+++ resources/views/components/sections/site/hero.blade.php
@@ -1,65 +1,4 @@
-{{--<div class="bg-gray-900">--}}
-{{--  <header class="absolute inset-x-0 top-0 z-50">--}}
-{{--    <nav class="flex items-center justify-between p-6 lg:px-8" aria-label="Global">--}}
-{{--      <div class="flex lg:flex-1">--}}
-{{--        <a href="#" class="-m-1.5 p-1.5">--}}
-{{--          <span class="sr-only">Your Company</span>--}}
-{{--          <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="">--}}
-{{--        </a>--}}
-{{--      </div>--}}
-{{--      <div class="flex lg:hidden">--}}
-{{--        <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-400">--}}
-{{--          <span class="sr-only">Open main menu</span>--}}
-{{--          <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">--}}
-{{--            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />--}}
-{{--          </svg>--}}
-{{--        </button>--}}
-{{--      </div>--}}
-{{--      <div class="hidden lg:flex lg:gap-x-12">--}}
-{{--        <a href="#" class="text-sm font-semibold leading-6 text-white">Product</a>--}}
-{{--        <a href="#" class="text-sm font-semibold leading-6 text-white">Features</a>--}}
-{{--        <a href="#" class="text-sm font-semibold leading-6 text-white">Marketplace</a>--}}
-{{--        <a href="#" class="text-sm font-semibold leading-6 text-white">Company</a>--}}
-{{--      </div>--}}
-{{--      <div class="hidden lg:flex lg:flex-1 lg:justify-end">--}}
-{{--        <a href="#" class="text-sm font-semibold leading-6 text-white">Log in <span aria-hidden="true">&rarr;</span></a>--}}
-{{--      </div>--}}
-{{--    </nav>--}}
-{{--	  <!-- Mobile menu, show/hide based on menu open state. -->--}}
-{{--    <div class="lg:hidden" role="dialog" aria-modal="true">--}}
-{{--      <!-- Background backdrop, show/hide based on slide-over state. -->--}}
-{{--      <div class="fixed inset-0 z-50"></div>--}}
-{{--      <div class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-gray-900 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-white/10">--}}
-{{--        <div class="flex items-center justify-between">--}}
-{{--          <a href="#" class="-m-1.5 p-1.5">--}}
-{{--            <span class="sr-only">Your Company</span>--}}
-{{--            <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="">--}}
-{{--          </a>--}}
-{{--          <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-400">--}}
-{{--            <span class="sr-only">Close menu</span>--}}
-{{--            <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">--}}
-{{--              <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />--}}
-{{--            </svg>--}}
-{{--          </button>--}}
-{{--        </div>--}}
-{{--        <div class="mt-6 flow-root">--}}
-{{--          <div class="-my-6 divide-y divide-gray-500/25">--}}
-{{--            <div class="space-y-2 py-6">--}}
-{{--              <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-800">Product</a>--}}
-{{--              <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-800">Features</a>--}}
-{{--              <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-800">Marketplace</a>--}}
-{{--              <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-800">Company</a>--}}
-{{--            </div>--}}
-{{--            <div class="py-6">--}}
-{{--              <a href="#" class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-white hover:bg-gray-800">Log in</a>--}}
-{{--            </div>--}}
-{{--          </div>--}}
-{{--        </div>--}}
-{{--      </div>--}}
-{{--    </div>--}}
-{{--  </header>--}}
-
-  <section class="relative isolate  min-h-[1080px]">
+<section class="relative isolate  min-h-100vh flex items-center">
     <div class="absolute h-full w-full " style="
 background: linear-gradient(180deg, #1C211B 0%, rgba(28, 33, 27, 0.30) 100%), url('/images/362203938_717630203706652_3259959972436561142_n 2.jpg'), lightgray 0 0 / 123.676% 123.611% no-repeat;
 background-repeat: no-repeat;
@@ -67,40 +6,33 @@ background-position: center bottom;
 background-size: cover;
 background-blend-mode: hard-light, normal;"></div>
 
-	  <a href="" class="absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-[58px]">
- <svg width="154" height="156" viewBox="0 0 154 156" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M2.10749 80.0537C21.9845 80.0537 41.0473 87.9498 55.1025 102.005C69.1576 116.06 77.0537 135.123 77.0537 155C77.0537 135.123 84.9499 116.06 99.005 102.005C113.06 87.9498 132.123 80.0537 152 80.0537"
-	  stroke="#ABE85E" stroke-width="4" stroke-linecap="round"/>
-<path d="M77 154L77 116.064L77 2" stroke="#ABE85E" stroke-width="4" stroke-linecap="round"/>
-</svg>
-	  </a>
+    <a href="" class="absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-[58px]">
+        <svg width="154" height="156" viewBox="0 0 154 156" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M2.10749 80.0537C21.9845 80.0537 41.0473 87.9498 55.1025 102.005C69.1576 116.06 77.0537 135.123 77.0537 155C77.0537 135.123 84.9499 116.06 99.005 102.005C113.06 87.9498 132.123 80.0537 152 80.0537"
+                  stroke="#ABE85E" stroke-width="4" stroke-linecap="round"/>
+            <path d="M77 154L77 116.064L77 2" stroke="#ABE85E" stroke-width="4" stroke-linecap="round"/>
+        </svg>
+    </a>
 
 
-    <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
-      <div class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-		   style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
-    </div>
+
     <div class="mx-auto max-w-[1150px] py-32 sm:py-48 lg:pb-40 lg:pt-44 relative">
-{{--      <div class="hidden sm:mb-8 sm:flex sm:justify-center">--}}
-		{{--        <div class="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-400 ring-1 ring-white/10 hover:ring-white/20">--}}
-		{{--          Announcing our next round of funding. <a href="#" class="font-semibold text-white"><span class="absolute inset-0" aria-hidden="true"></span>Read more <span aria-hidden="true">&rarr;</span></a>--}}
-		{{--        </div>--}}
-		{{--      </div>--}}
-		<div class="text-center">
-			<img class="mb-[20px] mx-auto" src="{{ asset('images/logo-therien.svg') }}" alt="" width="275">
-        <h1 class="text-[96px] leading-[1]  text-white font-display">Transformez vos rêves <br>
-en espaces extraordinaires.</h1>
-        <p class="mt-4 leading-[1.3] text-white text-[24px] ">Nous sculptons des espaces uniques qui allient <br> créativité, expertise et beauté naturelle.</p>
-        <div class="mt-14 flex items-center justify-center gap-x-6">
-			<x-button theme="large" class="leading-[1.1]">Faire un devis</x-button>
+        <div class="text-center">
+            <img class="mb-[20px] mx-auto" src="{{ asset('images/logo-therien.svg') }}" alt="" width="275">
+            <h1 class="text-[96px] leading-[1]  text-white font-display">Transformez vos rêves <br>
+                en espaces extraordinaires.</h1>
+            <p class="mt-4 leading-[1.3] text-white text-[24px] ">Nous sculptons des espaces uniques qui allient <br>
+                créativité, expertise et beauté naturelle.</p>
+            <div class="mt-14 flex items-center justify-center gap-x-6">
+                <x-button theme="large" class="leading-[1.1]">Faire un devis</x-button>
+            </div>
         </div>
-      </div>
     </div>
-   	<svg class="absolute right-[60px] bottom-[50px]" width="60" height="60" viewBox="0 0 60 60" fill="none"
-		 xmlns="http://www.w3.org/2000/svg">
-<path d="M30.0665 0C13.1274 0 0 12.3805 0 29.1027C0 37.8491 3.59395 45.4076 9.44395 50.6288C10.454 51.5373 10.246 52.0635 10.4177 57.6731C10.4313 58.066 10.5407 58.4497 10.7366 58.7906C10.9324 59.1315 11.2087 59.4193 11.5413 59.6289C11.8739 59.8386 12.2527 59.9636 12.6447 59.9932C13.0367 60.0228 13.43 59.956 13.7903 59.7986C20.1907 56.9799 20.273 56.7573 21.3581 57.0525C39.9012 62.1564 60 50.2889 60 29.1027C60 12.3805 47.0069 0 30.0665 0ZM48.1198 22.3959L39.2891 36.3769C38.9557 36.9026 38.5172 37.3538 38.0012 37.7021C37.4853 38.0505 36.9029 38.2885 36.2907 38.4013C35.6784 38.5141 35.0495 38.4992 34.4433 38.3575C33.8371 38.2159 33.2666 37.9505 32.7677 37.5781L25.7419 32.3194C25.4279 32.0838 25.0458 31.9565 24.6532 31.9565C24.2606 31.9565 23.8786 32.0838 23.5645 32.3194L14.0843 39.5101C12.819 40.4694 11.1617 38.9536 12.0145 37.6144L20.8452 23.6335C21.1785 23.1076 21.6169 22.6563 22.1329 22.3078C22.6488 21.9594 23.2312 21.7213 23.8435 21.6085C24.4558 21.4957 25.0848 21.5106 25.691 21.6524C26.2973 21.7941 26.8677 22.0596 27.3665 22.4322L34.3899 27.6897C34.704 27.9253 35.086 28.0526 35.4786 28.0526C35.8712 28.0526 36.2533 27.9253 36.5673 27.6897L46.0524 20.5063C47.3153 19.5409 48.9726 21.0555 48.1198 22.3959Z"
-	  fill="#ABE85E"/>
-</svg>
+    <svg class="absolute right-[60px] bottom-[50px]" width="60" height="60" viewBox="0 0 60 60" fill="none"
+         xmlns="http://www.w3.org/2000/svg">
+        <path d="M30.0665 0C13.1274 0 0 12.3805 0 29.1027C0 37.8491 3.59395 45.4076 9.44395 50.6288C10.454 51.5373 10.246 52.0635 10.4177 57.6731C10.4313 58.066 10.5407 58.4497 10.7366 58.7906C10.9324 59.1315 11.2087 59.4193 11.5413 59.6289C11.8739 59.8386 12.2527 59.9636 12.6447 59.9932C13.0367 60.0228 13.43 59.956 13.7903 59.7986C20.1907 56.9799 20.273 56.7573 21.3581 57.0525C39.9012 62.1564 60 50.2889 60 29.1027C60 12.3805 47.0069 0 30.0665 0ZM48.1198 22.3959L39.2891 36.3769C38.9557 36.9026 38.5172 37.3538 38.0012 37.7021C37.4853 38.0505 36.9029 38.2885 36.2907 38.4013C35.6784 38.5141 35.0495 38.4992 34.4433 38.3575C33.8371 38.2159 33.2666 37.9505 32.7677 37.5781L25.7419 32.3194C25.4279 32.0838 25.0458 31.9565 24.6532 31.9565C24.2606 31.9565 23.8786 32.0838 23.5645 32.3194L14.0843 39.5101C12.819 40.4694 11.1617 38.9536 12.0145 37.6144L20.8452 23.6335C21.1785 23.1076 21.6169 22.6563 22.1329 22.3078C22.6488 21.9594 23.2312 21.7213 23.8435 21.6085C24.4558 21.4957 25.0848 21.5106 25.691 21.6524C26.2973 21.7941 26.8677 22.0596 27.3665 22.4322L34.3899 27.6897C34.704 27.9253 35.086 28.0526 35.4786 28.0526C35.8712 28.0526 36.2533 27.9253 36.5673 27.6897L46.0524 20.5063C47.3153 19.5409 48.9726 21.0555 48.1198 22.3959Z"
+              fill="#ABE85E"/>
+    </svg>
 
-  </section>
-{{--</div>--}}
+</section>
+
